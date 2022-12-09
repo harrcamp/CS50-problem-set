a1e5ba2b1546afb0855e5e13440ec90978f4d350
harry <hpc710@gmail.com>
2022-12-08 18:01:03 -0600

    test

---
+++ meow.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// while loop declare variable outside loop
+// int main(void)
+// {
+//     int i = 0;
+//     while (i < 3)
+//     {
+//         printf("meow\n");
+//         i++;
+//     }
+// }
+
+for loop declare variable inside loop
+{
+   for (int i = 0; i < 3; i++)
+   {
+       printf("meow\n");
+   }
+}
